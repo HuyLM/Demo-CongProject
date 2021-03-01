c18e264df48c028fe17a41e749bf6c524cf1d2b9
huylm <lemanhhuy.khmt@gmail.com>
2021-03-01 21:50:28 +0700

    first commit

---
+++ Assets/test.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class test : MonoBehaviour {
+    public TextMeshProUGUI txt;
+    private void Start()
+    {
+        txt.text = $"Lê Mạnh Huy và Nguyễn Đình Công";
+        /*
+        Quay lại;
+        Thêm mới;
+        Ngày lập;
+        Họ tên;
+        Tên hàng;
+        Đơn giá(nghìn VNĐ);
+        Thành Tiền(nghìn VNĐ);
+        Lưu ý;
+        Ghi chú;
+        Tổng cộng;
+        Chú ý đây là chú ý. Cảm ơn đã chú ý vào chú ý. Sự chú ý của bạn sẽ nhận được sự chú ý của chúng tôi;
+        Đã thanh toán;
+        Dư nợ;
+        Trạng thái;
+        Đã nhận hàng;
+        Đã xong;
+        Chờ nhận;
+        Bỏ hàng;
+        
+         */
+    }
+}
